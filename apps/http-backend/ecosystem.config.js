5037f9a1831e03ca8e9d181e46b963cbd4acb070
dheeraj1717 <dheerajjangid1717@gmail.com>
2025-12-28 14:39:56 +0530

    Update start script to use dotenv

--- apps/http-backend/ecosystem.config.js
+++ apps/http-backend/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "http-backend",
       script: "npm",
       args: "run start",
+      cwd: "/home/ubuntu/Sketch/apps/http-backend",
       env: {
         NODE_ENV: "production",
         PORT: 3001,
